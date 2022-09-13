80e132434887c0341b5c02480df2f5d0579c3177
xxl4tomxu98 <tomxu@udel.edu>
2022-09-13 18:38:03 -0400

    add both resources and containers

--- restaurants/hungry_philosopher.py
+++ restaurants/hungry_philosopher.py
@@ -9,12 +9,15 @@ class Philosoper():
     T1 = 15
     # time to pick up the other chopstick
     DT = 3
+    # Single meal size
+    PORTION = 20
 
-    def __init__(self, env, chopsticks, my_id, DIAG=True):        
+    def __init__(self, env, chopsticks, my_id, bowl=None, DIAG=False):        
         self.env = env
-        self.chopsticks = chopsticks
+        self.chopsticks = sorted(chopsticks, key=id)
         self.id = my_id
         self.DIAG = DIAG
+        self.bowl = bowl
         self.waiting = 0
         # register the process with the environment
         env.process(self.run_the_party())
@@ -32,6 +35,9 @@ class Philosoper():
         rq2 = self.chopsticks[1].request()
         yield rq2
         self.diag("obtained another chopstick")
+        if self.bowl is not None:
+            yield self.bowl.get(self.PORTION)
+            self.diag("reserved food")
         self.waiting += self.env.now - start_waiting
         return rq1, rq2
 
@@ -61,14 +67,33 @@ class Philosoper():
             print(f'P{self.id}, {message}, @{self.env.now}')  
 
 
+class Chef():
+    # mean time for cooking for replenishing
+    T2 = 150
+
+    def __init__(self, env, bowl):
+        self.env = env
+        self.bowl = bowl
+        env.process(self.replenish())
+
+    def replenish(self):
+        while True:
+            yield self.env.timeout(self.T2)
+            if self.bowl.level < self.bowl.capacity:
+                yield self.bowl.put(self.bowl.capacity - self.bowl.level)
+
+
 def simulate(n, t):
     '''
     Simulate the system of n philosopher for up to t time units
     and return the average waiting time
     '''
     env = simpy.Environment()
+    rice_bowl = simpy.Container(env, init=1000, capacity=1000)
+    chef = Chef(env, rice_bowl)
     chopsticks = [simpy.Resource(env, capacity=1) for i in range(n)]
-    philosophers = [Philosoper(env, (chopsticks[i], chopsticks[(i+1)%n]), i) for i in range(n)]
+    philosophers = [Philosoper(env, (chopsticks[i], chopsticks[(i+1)%n]), i, rice_bowl)
+                    for i in range(n)]
     env.run(until=t)
     return sum(ph.waiting for ph in philosophers)/n
 
