1c19770d935f246fbf2d5b9c4d3df2190141fd81
xxl4tomxu98 <tomxu@udel.edu>
2022-09-13 20:00:12 -0400

    add both resources and containers and wait limits

--- restaurants/hungry_philosopher.py
+++ restaurants/hungry_philosopher.py
@@ -2,71 +2,6 @@ import simpy
 import random
 
 
-class Philosoper():
-    # mean time for thinking
-    T0 = 10
-    # mean time for eating
-    T1 = 15
-    # time to pick up the other chopstick
-    DT = 3
-    # Single meal size
-    PORTION = 20
-
-    def __init__(self, env, chopsticks, my_id, bowl=None, DIAG=False):        
-        self.env = env
-        self.chopsticks = sorted(chopsticks, key=id)
-        self.id = my_id
-        self.DIAG = DIAG
-        self.bowl = bowl
-        self.waiting = 0
-        # register the process with the environment
-        env.process(self.run_the_party())
-
-    # request resources
-    def get_hungry(self):
-        start_waiting = self.env.now
-        self.diag("requested chopstick")
-        rq1 = self.chopsticks[0].request()
-        yield rq1
-        self.diag("obtained chopstick")
-        # block generator DT between 2 chopsticks
-        yield self.env.timeout(self.DT)
-        self.diag("requested another chopstick")
-        rq2 = self.chopsticks[1].request()
-        yield rq2
-        self.diag("obtained another chopstick")
-        if self.bowl is not None:
-            yield self.bowl.get(self.PORTION)
-            self.diag("reserved food")
-        self.waiting += self.env.now - start_waiting
-        return rq1, rq2
-
-    # do everything
-    def run_the_party(self):
-        while True:
-            # Thinking time is exponential distribution
-            thinking_delay = random.expovariate(1/self.T0)
-            # block generator for the thinking delay time
-            yield self.env.timeout(thinking_delay)
-            # suspend main process getting hungry yield all events 
-            get_hungry_p = self.env.process(self.get_hungry())
-            rq1, rq2 = yield get_hungry_p
-            # Eating time is also exponential 
-            eating_delay = random.expovariate(1/self.T1)
-            # block generator for eating delay time
-            yield self.env.timeout(eating_delay)
-            # use the request handlers returned from subprocess
-            # get_hungry() to release the resources
-            self.chopsticks[0].release(rq1)
-            self.chopsticks[1].release(rq2)
-            self.diag("release the chopsticks")
-        
-    # diagnosis
-    def diag(self, message):
-        if self.DIAG:
-            print(f'P{self.id}, {message}, @{self.env.now}')  
-
-
 class Chef():
     # mean time for cooking for replenishing
     T2 = 150
@@ -81,6 +16,90 @@ class Chef():
             yield self.env.timeout(self.T2)
             if self.bowl.level < self.bowl.capacity:
                 yield self.bowl.put(self.bowl.capacity - self.bowl.level)
+
+
+class Philosoper():
+    # mean time for thinking
+    T0 = 10
+    # mean time for eating
+    T1 = 15
+    # time to pick up the other chopstick
+    DT = 3
+    # Single meal size
+    PORTION = 20
+    # limit the waiting time 
+    MAX_WAIT = (Chef.T2)/2
+
+    def __init__(self, env, chopsticks, my_id, bowl=None, DIAG=False):        
+        self.env = env
+        self.chopsticks = sorted(chopsticks, key=id)
+        self.id = my_id
+        self.DIAG = DIAG
+        self.bowl = bowl
+        self.waiting = 0
+        # register the process with the environment
+        env.process(self.run_the_party())
+
+    # request resources
+    def get_hungry(self, meal_size):
+        start_waiting = self.env.now
+        self.diag("requested chopstick")
+        rq1 = self.chopsticks[0].request()
+        yield rq1
+        self.diag("obtained chopstick")
+        # block generator DT between 2 chopsticks
+        yield self.env.timeout(self.DT)
+        self.diag("requested another chopstick")
+        rq2 = self.chopsticks[1].request()
+        yield rq2
+        self.diag("obtained another chopstick")
+        if self.bowl is not None:
+            # container request
+            request = self.bowl.get(meal_size)
+            # yield self.bowl.get(self.PORTION)
+            # yield any of the two events
+            yield request | self.env.timeout(self.MAX_WAIT)
+            if request.processed:
+                self.diag("reserved food")
+            else:
+                # timeout
+                self.diag("gave up")
+                self.waiting += self.env.now - start_waiting
+                yield simpy.Event(self.env).fail(ValueError(rq1, rq2))
+        # Either no bowl or no timeout!
+        self.waiting += self.env.now - start_waiting
+        return rq1, rq2
+
+    # do everything
+    def run_the_party(self):
+        meal_size = self.PORTION
+        while True:
+            # Thinking time is exponential distribution
+            thinking_delay = random.expovariate(1/self.T0)
+            # block generator for the thinking delay time
+            yield self.env.timeout(thinking_delay)
+            # suspend main process getting hungry yield all events 
+            get_hungry_p = self.env.process(self.get_hungry(meal_size))
+            try:
+                rq1, rq2 = yield get_hungry_p
+                # Eating time is also exponential 
+                eating_delay = random.expovariate(1/self.T1)
+                # block generator for eating delay time
+                yield self.env.timeout(eating_delay)
+                meal_size = self.PORTION
+            except ValueError as values:  #timeout
+                rq1, rq2 = values.args
+                meal_size += self.PORTION
+            # use the request handlers returned from subprocess
+            # get_hungry() to release the resources
+            self.chopsticks[0].release(rq1)
+            self.chopsticks[1].release(rq2)
+            self.diag("release the chopsticks")
+        
+    # diagnosis
+    def diag(self, message):
+        if self.DIAG:
+            print(f'P{self.id}, {message}, @{self.env.now}')  
 
 
 def simulate(n, t):
