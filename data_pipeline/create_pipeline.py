2c1c981daa42dcf640337f8781e77e0f70be9b68
xxl4tomxu98 <tomxu@udel.edu>
2022-09-21 19:05:00 -0400

    only one time through iterator

--- data_pipeline/create_pipeline.py
+++ data_pipeline/create_pipeline.py
@@ -13,12 +13,19 @@ cols = next(list_line)
 company_dicts = (dict(zip(cols, data)) for data in list_line)
 # filter out company’s series A funding amounts when its round is series A
 funding = (
-            [int(company_dict["raisedAmt"]), int(company_dict["numEmps"] or 0)]
+            int(company_dict["raisedAmt"])
             for company_dict in company_dicts
-            if company_dict["round"] == "b"
+            if company_dict["round"] == "c"
           )
+
+employees = (
+              int(company_dict["numEmps"] or 0)
+              for company_dict in company_dicts
+              if company_dict["round"] == "c"
+            )
+
 # iteration process by calling sum() to get the total amount of series A funding found in the CSV
-total_series_a = sum([fund[0] for fund in funding])
-total_num_emp = sum([fund[1] for fund in funding]) 
+# note that once going through the iterator once, the second time employees become 0
+total_series_a, total_num_emp = sum(funding), sum(employees)
 # generate final results
 print(f"Total series A fundraising: ${total_series_a} Total employees: {total_num_emp}")
